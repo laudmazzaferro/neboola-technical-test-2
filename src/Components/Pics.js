097fad6ae44ad02286ee764702b0a63368143bfd
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-11-26 14:03:58 +0100

    Create html structure of pics component and paint the list of pictures

--- src/Components/Pics.js
+++ src/Components/Pics.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-class Pics extends  React.Component {
-  render(){
+const Pics = (props) => {
+  const {pics} = props
     return(
-      <div></div>
+      <div className="pics__container">
+        <ul className="pics__list">
+        {pics.map(item => {
+          return(
+            <li className="pic__item" key={item.id}>
+              <img className="pic__img" src={item.download_url} alt=""/>
+              <h2 className="pic__author">{item.author}</h2>
+            </li>
+          )
+        })} 
+        </ul>
+      </div>
     );
-  }
+
 }
 
 export default Pics;
