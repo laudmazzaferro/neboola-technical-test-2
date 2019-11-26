5fd352f77eb79321526526369ec10410eb944e55
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-11-26 13:43:46 +0100

    Write title in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Header = (props) => {
   return(
-    <div>header</div>
+    <div className="header">
+      <h1 className="title">Cool Pics</h1>
+    </div>
   );
 };
 
